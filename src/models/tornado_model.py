18585795a6c934041f727aee4380cc57ebb27e88
Veritec2 <nntuck2@gmail.com>
2025-07-14 20:45:27 -0700

    (As Colleague A) feat: Add basic model structure

---
+++ src/models/tornado_model.py
@@ -0,0 +1,13 @@
+﻿"""
+Model module for tornado prediction
+"""
+
+def train_model(data):
+    """Train the tornado predicition model"""
+    print("Training model...")
+    # TODO: Implement training
+    return {"accuracy": 0.0}
+
+def predict(model, features):
+    """Make predictions"""
+    return []
