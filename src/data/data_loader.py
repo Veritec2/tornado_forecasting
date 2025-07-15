f227ee0eff5336419111ac59b6f85536d250117d
Veritec2 <nntuck2@gmail.com>
2025-07-14 17:41:35 -0700

    feat: Add basic data loader module with load and validate functions

---
+++ src/data/data_loader.py
@@ -0,0 +1,15 @@
+﻿"""
+Data loader module for tornado forecasting project
+"""
+
+def load_tornado_data(filepath):
+    """Load tornado data from CSV file"""
+    print(f"Loading data from {filepath}")
+    # TODO: Implement actual loading logic
+    return []
+
+def validate_data(data):
+    """Validate loaded data"""
+    print("Validating data...")
+    # TODO: Add validation logic
+    return True
