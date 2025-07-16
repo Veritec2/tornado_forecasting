4f688c995f8c3ced867f69a9079e74ed66b699db
Veritec2 <nntuck2@gmail.com>
2025-07-16 09:58:31 -0700

    feat: Add config loader

---
+++ src/config_loader.py
@@ -0,0 +1,12 @@
+﻿"""
+Configuration loader with an intentional bug
+"""
+
+def load_config(config_path):
+    """Load configuration file"""
+    # BUG: This will fail - missing 'encoding' parameter
+    with open(config_path, 'r') as f:
+        config = f.read()
+    
+    # BUG: Wrong variable name
+    return configuration  # Should be 'config'
